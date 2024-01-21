ec8c570c80b63695a8fc2d0fba665b3fa8a903be
HariKrishnaYadav <Harikrishna.bhuit15@gmail.com>
2024-01-21 19:30:50 +0530

    Check if product of first N natural numbers is divisible by their sum

--- DSAPractice/src/hky/practice/ProductSumDivisible.java
+++ DSAPractice/src/hky/practice/ProductSumDivisible.java
@@ -1,4 +1,25 @@
 package hky.practice;
 
+import java.util.Scanner;
+
 public class ProductSumDivisible {
+    private static boolean isDivisible(int n) {
+        int product = 1, sum = 0;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+            sum += i;
+        }
+        return (product % sum == 0);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (isDivisible(n)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+
 }
